a516e537af300e0d76f28dbf073ee3852f862e42
Valentina <maggio.vale@gmail.com>
2022-03-08 18:22:52 +0000

    Created testing_infrastructure_spec
    Co-authored-by: Henry Ferrari <HCVF@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -2,9 +2,16 @@ require 'sinatra/base'
 require 'sinatra/reloader' 
 
 class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+  
   get '/' do
-    "Hello Battle!"
+    "Testing infrastructure working!"
   end
 
   run! if app_file == $0
 end
+
+
+# 
