0b4e300c97c142c30653ce2a4bf8864b62464381
Valentina <maggio.vale@gmail.com>
2022-03-10 16:13:43 +0000

    Created web_helper to extract repetitve user actions
    Co-authored-by: Hal Wollocombe <Halwoll@users.noreply.github.com>

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View player hit points' do
   scenario 'see Player 2 hit points' do
-    visit('/')
-    fill_in 'player1', with: 'Hal'
-    fill_in 'player2', with: 'Valentina'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content('Player 2: Valentina, Hit points: 20')
   end
 end
--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,12 +1,6 @@
 feature 'Testing form' do
   scenario 'players can add their names' do
-    visit('/')
-    fill_in 'player1', with: 'Henry'
-    fill_in 'player2', with: 'Valentina'
-    click_button 'Submit'
-
-    #save_and_open_page
-    
-    expect(page).to have_text('Henry vs. Valentina')
+    sign_in_and_play
+    expect(page).to have_text('Hal vs. Valentina')
   end
 end
