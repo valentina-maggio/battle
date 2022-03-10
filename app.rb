dc7c8f5d21492cf37d55ba689d1e410b1e2bbe88
Valentina <maggio.vale@gmail.com>
2022-03-10 16:52:06 +0000

    Added attack button and get route
    Co-authored-by: Hal Wollocombe <Halwoll@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -24,5 +24,11 @@ class Battle < Sinatra::Base
     erb(:play)
   end
 
+  get '/attack' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb(:attack)
+  end
+
   run! if app_file == $0
 end
